e1888ba2e3a041866c1c5f5c3b80f9e8586ea73d
skullbreakerr <pateldharmiiik2002@gmail.com>
2024-05-04 12:51:29 +0530

    Added the Firebase Authentication in the site

---
+++ firebase.config.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyDdh4wwt4bYqwUq0x_HG5TgJh59Aunm9JY",
+
+    authDomain: "farm-fresh-e2d80.firebaseapp.com",
+
+    projectId: "farm-fresh-e2d80",
+
+    storageBucket: "farm-fresh-e2d80.appspot.com",
+
+    messagingSenderId: "573325688304",
+
+    appId: "1:573325688304:web:ec1f2a2e14bda8f5e3f135"
+
+  };
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
